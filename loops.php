da6cdcd472049bf4c66fcac0957d8015a1d831f2
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-02-02 15:16:26 +0000

    Implement elseif statement into current loop

--- loops.php
+++ loops.php
@@ -1,9 +1,11 @@
 <?php
-  $age = 18;
+  $age = 16;
 
   if ($age >= 18) {
       echo "You are an adult.";
+  } elseif ($age >= 13) {
+      echo "You are a teenager.";
   } else {
-      echo "You are a minor.";
+      echo "You are a child.";
   }
 ?>
