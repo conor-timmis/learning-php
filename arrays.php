e88e454fec50251e4786cdb876f4f20e7644de7e
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2025-02-02 16:00:16 +0000

    Add elements to the indexed array

--- arrays.php
+++ arrays.php
@@ -2,6 +2,15 @@
   // Indexed Array
   $fruits = ["Apple", "Banana", "Cherry"];
 
+  echo "Indexed Array:<br>";
   echo $fruits[0];  // Output: Apple
+  echo "<br>";
   echo $fruits[1];  // Output: Banana
+  echo "<br>";
+  
+  // Adding an element to the indexed array
+  $fruits[] = "Orange";
+  echo $fruits[3];
+  echo "<br><br>";
+  
 ?>
